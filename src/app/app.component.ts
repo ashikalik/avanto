2b76f29cc10fa693f23622548cfed3262457f8b3
ashik <ashikalik@gmail.com>
2019-09-05 19:59:36 +0530

    switch languages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,30 @@
-import { Component } from '@angular/core';
-import {Directionality} from '@angular/cdk/bidi';
-import {TranslateService} from '@ngx-translate/core';
+import { Component } from "@angular/core";
+import { Directionality } from "@angular/cdk/bidi";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'evento';
+  title = "evento";
   private isRtl: boolean;
   public direction: string;
-
+  private translate;
 
   constructor(dir: Directionality, translate: TranslateService) {
-    this.direction = 'ltr';
-    this.isRtl = dir.value === 'rtl';
-    translate.setDefaultLang('en');
-    translate.use('en');
+    this.translate = translate;
+    this.direction = "ltr";
+    this.isRtl = dir.value === "rtl";
+    translate.setDefaultLang("en");
+    translate.use("en");
   }
 
   onDirectionChanged(newDirecion: string) {
-   
     console.log(newDirecion);
     this.direction = newDirecion;
-
-   
-
+    if (this.direction === "RTL") this.translate.use("ar");
+    else this.translate.use("en");
   }
-
 }
