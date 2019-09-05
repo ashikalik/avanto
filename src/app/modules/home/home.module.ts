ea0da6890cae8011dc775312f6b9a7994be1d1c1
ashik <ashikalik@gmail.com>
2019-09-05 22:01:13 +0530

    started home module

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SearchEventComponent } from './search-event/search-event.component';
+import { LatestEventsComponent } from './latest-events/latest-events.component';
+import { PartnersComponent } from './partners/partners.component';
+
+
+
+@NgModule({
+  declarations: [HomePageComponent, SearchEventComponent, LatestEventsComponent, PartnersComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
