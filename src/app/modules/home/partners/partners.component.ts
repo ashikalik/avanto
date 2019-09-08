5f71c648c898a43d00bd0c570763379f9af8ec4a
ashik <ashikalik@gmail.com>
2019-09-08 17:51:21 +0530

    added partners carousel

--- src/app/modules/home/partners/partners.component.ts
+++ src/app/modules/home/partners/partners.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from "@angular/core";
 
 @Component({
-  selector: 'app-partners',
-  templateUrl: './partners.component.html',
-  styleUrls: ['./partners.component.scss']
+  selector: "app-partners",
+  templateUrl: "./partners.component.html",
+  styleUrls: ["./partners.component.scss"]
 })
-export class PartnersComponent implements OnInit {
+export class PartnersComponent implements OnInit, AfterViewInit {
+  constructor() {}
 
-  constructor() { }
+  ngAfterViewInit() {
+    $("#carouselExample").on("slide.bs.carousel", function(e) {
+      /*
+      
+      CC 2.0 License Iatek LLC 2018
+      Attribution required
+      
+      */
+      console.log("dd");
 
-  ngOnInit() {
+      var $e = $(e.relatedTarget);
+      var idx = $e.index();
+      var itemsPerSlide = 4;
+      var totalItems = $(".carousel-item").length;
+
+      if (idx >= totalItems - (itemsPerSlide - 1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i = 0; i < it; i++) {
+          // append slides to end
+          if (e.direction == "left") {
+            $(".carousel-item")
+              .eq(i)
+              .appendTo(".carousel-inner");
+          } else {
+            $(".carousel-item")
+              .eq(0)
+              .appendTo(".carousel-inner");
+          }
+        }
+      }
+    });
   }
 
+  ngOnInit() {}
 }
