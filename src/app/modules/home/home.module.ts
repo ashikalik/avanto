dfaf828ff45af9cb24d0ebf66549476aac72bd1a
ashik <ashikalik@gmail.com>
2019-09-12 12:17:12 +0530

    removed extra wrapper component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,7 +5,6 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { LatestEventsComponent } from './latest-events/latest-events.component';
 import { PartnersComponent } from './partners/partners.component';
 import { FormsModule } from '@angular/forms';
-import { SearchCreateEventComponent } from './search-create-event/search-create-event.component';
 import { CreateEventComponent } from './create-event/create-event.component';
 import { SearchEventModule } from '../../shared/search-event/search-event.module';
 import { EventListModule } from '../../shared/event-list/event-list.module';
@@ -19,7 +18,6 @@ import { CoreModule } from '../../core/core.module';
     LatestEventsComponent,
     PartnersComponent,
     CreateEventComponent,
-    SearchCreateEventComponent,
     StatisticsComponent
   ],
   imports: [
