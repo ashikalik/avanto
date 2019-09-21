b10f9dd5d6caba4d2c43377d0ce6a04c46fd92ea
ashik <ashikalik@gmail.com>
2019-09-21 21:39:51 +0530

    Password update component and responsive fixes

--- src/app/modules/profile/update-password/update-password.component.ts
+++ src/app/modules/profile/update-password/update-password.component.ts
@@ -1,15 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators
+} from "@angular/forms";
+import { ServerError } from "src/app/core/model/common/server-error";
+import { ProfileService } from "../../../core/api-services/profile.service";
+import { UserAuthService } from "src/app/core/service/user-auth.service";
+import { UpdatePasswordRequest } from 'src/app/core/model/user/update-password-request';
+import { UserProfileData } from "../../../core/model/user/user-profile-data";
+
 
 @Component({
-  selector: 'app-update-password',
-  templateUrl: './update-password.component.html',
-  styleUrls: ['./update-password.component.scss']
+  selector: "app-update-password",
+  templateUrl: "./update-password.component.html",
+  styleUrls: ["./update-password.component.scss"]
 })
 export class UpdatePasswordComponent implements OnInit {
 
-  constructor() { }
+  @Input() userProfileData: UserProfileData;
+
+  public passwordForm: FormGroup;
+  public passwordError: ServerError;
+  public passwordSuccess: boolean;
+
+  constructor(
+    public formBuilder: FormBuilder,
+    public profileService: ProfileService,
+    public userAuthService: UserAuthService
+  ) {}
 
   ngOnInit() {
+    this.initPasswordForm();
   }
+
+  public initPasswordForm() {
+    this.passwordForm = this.formBuilder.group(
+      {
+        current_password: ["", Validators.compose([Validators.required])],
+        new_password: [
+          "",
+          Validators.compose([Validators.required, Validators.minLength(6)])
+        ],
+        confirm_password: [
+          "",
+          Validators.compose([Validators.required, Validators.minLength(6)])
+        ]
+      },
+      {
+        validator: this.passwordConfirm
+      }
+    );
+  }
+
+  public passwordConfirm(AC: AbstractControl): { invalid: boolean } {
+    if (AC.get("new_password").value !== AC.get("confirm_password").value) {
+      return { invalid: true };
+    }
+  }
+
+  public updatePassword(form: FormGroup) {
+    let body: UpdatePasswordRequest = form.value;
+    this.passwordError = null;
+    this.passwordSuccess = false;
+    this.profileService.updatePassword(body, this.userProfileData.data.user_id).subscribe(res => {
+        this.userAuthService.setToken(res.token);
+        this.passwordSuccess = true;
+    }, err => {
+        this.passwordError = err.value.error;
+    });
+}
+
 
 }
