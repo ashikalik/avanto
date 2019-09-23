3936ae0b56a9292d77ac09ccc565144b7ec3a9ce
ashik <ashikalik@gmail.com>
2019-09-23 08:25:10 +0530

    personal info from style

--- src/app/modules/buy-ticket/personal-info/personal-info.component.ts
+++ src/app/modules/buy-ticket/personal-info/personal-info.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-personal-info',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personal-info.component.scss']
 })
 export class PersonalInfoComponent implements OnInit {
+  @Input('buyTicketForm') buyTicketForm : FormGroup;
 
   constructor() { }
 
